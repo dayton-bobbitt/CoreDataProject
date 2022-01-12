6fe0a98d30bc751e0151ebaaea66892e894c5b28
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-12 17:15:40 -0600

    Setup CoreData with SwiftUI

---
+++ CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Dayton Bobbitt on 1/12/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { _description, error in
+            if let error = error {
+                fatalError("Failed to load persistent store: \(error.localizedDescription)")
+            }
+        }
+    }
+}
