9a67e475b430e5dd5f85e941a1c2bf50a3daadfd
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-12 17:07:05 -0600

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Dayton Bobbitt on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
